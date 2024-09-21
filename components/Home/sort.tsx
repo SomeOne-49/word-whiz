e91ca1f6cc94d701068e5f2038ae8ae306e8c03e
Albaraa Alismaeel <albraa@vega4it.com>
2024-09-21 13:41:56 +0300

    test sort

--- components/Home/sort.tsx
+++ components/Home/sort.tsx
@@ -1,36 +1,16 @@
-// import { CardsFilter } from "@/constants";
-import Image from 'next/image';
-import { Button } from '../ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '../ui/dropdown-menu';
+// // import { CardsFilter } from "@/constants";
+// import Image from 'next/image';
+// import { Button } from '../ui/button';
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from '../ui/dropdown-menu';
 
 const HomeSort = () => {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="px-2">
-          <Image
-            src="/assets/icons/sort-colord.svg"
-            width={24}
-            height={24}
-            alt="sort"
-          />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" arrowPadding={30}>
-        <DropdownMenuItem>Filter</DropdownMenuItem>
-
-        {/* {CardsFilter.map((filter) => {
-          return (
-            <DropdownMenuItem key={filter.name}>{filter.name}</DropdownMenuItem>
-          );
-        })} */}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div>Test Sort</div>
   );
 };
 
