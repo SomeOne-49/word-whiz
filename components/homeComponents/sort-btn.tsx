2817173fc9171087f4146c15e8303f10de7a0221
Albaraa Alismaeel <albraa@vega4it.com>
2024-09-21 13:47:10 +0300

    sort Test

--- components/homeComponents/sort-btn.tsx
+++ components/homeComponents/sort-btn.tsx
@@ -1,9 +1,35 @@
+import { CardsFilter } from '@/constants';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
 const SortBtn = () => {
   return (
-    <>
-      SortBtn 
-    </>
-  )
-}
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="px-2">
+          <Image
+            src="/assets/icons/sort-colord.svg"
+            width={24}
+            height={24}
+            alt="sort"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" arrowPadding={30}>
+        <DropdownMenuItem>Filter</DropdownMenuItem>
+        {CardsFilter.map((filter) => {
+          return (
+            <DropdownMenuItem key={filter.name}>{filter.name}</DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 export default SortBtn;
