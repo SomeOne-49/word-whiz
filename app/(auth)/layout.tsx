5fe5ec67da812f9dbbfe011b5729d228922f684d
Albaraa Alismaeel <albraa@vega4it.com>
2024-09-22 08:00:24 +0300

    test screen

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -24,7 +24,7 @@ export default function AuthLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${baloo.className} relative h-screen`}>
+        <body className={`${baloo.className} relative h-svh`}>
           <div className="absolute left-0 top-0 z-10 size-full bg-black/50 backdrop-blur-sm" />
           <Image
             src="/assets/auth-bg.svg"
@@ -32,7 +32,6 @@ export default function AuthLayout({
             alt="books"
             className="object-cover"
           />
-          
         </body>
       </html>
     </ClerkProvider>
